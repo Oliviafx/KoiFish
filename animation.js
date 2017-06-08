69b58428cad073a5ac0bf5fe67c4ee3b8dfecfa7
Olivia Xiang <oliviafx@gmail.com>
2017-06-07 22:42:33 -0400

    attempts at water ripples part 2

--- animation.js
+++ animation.js
@@ -1,3 +1,4 @@
+/*
 var c = document.getElementById("myCanvas"); //food
 var ctx = c.getContext("2d");
 ctx.strokeStyle="#FF4500";
@@ -7,3 +8,40 @@ c.onclick = function(){
 	var Ycoord = event.clientY;
 	ctx.strokeRect(Xcoord, Ycoord, 1, 1);
 };
+*/
+
+$("div").click(function (e) {
+
+  // Remove any old one
+  $(".ripple").remove();
+
+  // Setup
+  var posX = $(this).offset().left,
+      posY = $(this).offset().top,
+      buttonWidth = $(this).width(),
+      buttonHeight =  $(this).height();
+
+  // Add the element
+  $(this).prepend("<span class='ripple'></span>");
+
+
+ // Make it round!
+  if(buttonWidth >= buttonHeight) {
+    buttonHeight = buttonWidth;
+  } else {
+    buttonWidth = buttonHeight; 
+  }
+
+  // Get the center of the element
+  var x = e.pageX - posX - buttonWidth / 2;
+  var y = e.pageY - posY - buttonHeight / 2;
+
+
+  // Add the ripples CSS and start the animation
+  $(".ripple").css({
+    width: buttonWidth,
+    height: buttonHeight,
+    top: y + 'px',
+    left: x + 'px'
+  }).addClass("rippleEffect");
+});
